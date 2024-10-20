8a8ffcfa4e9d7c32329ddea005ac9dd9f2d9d8b4
jannchoi <134582746+jannchoi@users.noreply.github.com>
2024-10-20 17:17:55 +0900

    Add files via upload

---
+++ Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by 최정안 on 9/7/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable{
+    let id: String
+    let name: String
+    let description: String
+}
