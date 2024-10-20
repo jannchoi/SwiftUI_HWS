e17d5706986a523fb0e62e1e2f5b18dcace24ab5
jannchoi <134582746+jannchoi@users.noreply.github.com>
2024-10-20 17:15:28 +0900

    Add files via upload

---
+++ RPSgame/RPSgame/RPSgameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPSgameApp.swift
+//  RPSgame
+//
+//  Created by 최정안 on 8/19/24.
+//
+
+import SwiftUI
+
+@main
+struct RPSgameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
